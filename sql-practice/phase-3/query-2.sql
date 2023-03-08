ebafaf07a922130bf2f83cbbea96567cff69bc5d
Mayolito Waje <mayolito.waje08@gmail.com>
2023-03-08 20:41:49 +0800

    improve query for finding hermione's cat's toys

--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,6 +1,5 @@
 -- Find All the Toys for Hermione's cats
 SELECT toys.name FROM toys
-  JOIN cats ON (cats.id = toys.cat_id)
-  JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+  JOIN cat_owners ON (cat_owners.cat_id = toys.cat_id)
   JOIN owners ON (cat_owners.owner_id = owners.id)
   WHERE owners.first_name = 'Hermione';
